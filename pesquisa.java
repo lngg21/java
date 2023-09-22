652ee55ad567456e1fc845ea424b1efe8a804389
Arthur Vinicius De Sena Moraes <123411782+lngg21@users.noreply.github.com>
2023-09-22 10:14:34 -0300

    pesquisa.java

--- pesquisa.java
+++ pesquisa.java
@@ -1,9 +1,9 @@
 public class pesquisa  {
     public static void main(String[] args){
-        int[] vet = {27, 19, 37, 15, 20, 17, 21, 12};
-        int procurado = 12;
+        int[] vet = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int procurado = 90;
         
-        int r = buscaSequencial(vet, procurado );
+        int r = buscaMelhorada(vet, procurado );
 
         if(r == -1){
             System.out.println("O elemento não está presente no vetor");
@@ -12,19 +12,39 @@ public class pesquisa  {
         }
     
     }
-    public static int buscaSequencial (double v[ ], double procurado, int n){
+    /*public static int buscaSequencial (int vet[ ], int procurado){
         int  i;
         boolean  achou = false;
-        for (i = 0; i < n; i++)  
-        {if (v[i] == procurado)  {
-            achou = true;break;
+        for (i = 0; i < vet.length; i++)  
+        {if (vet[i] == procurado)  {
+            achou = true;
+            break;
         }
     }
     if (achou == true){
         return i;     // retorna a posição onde encontrou a chave}   
      } else {
         return -1;  // retorna -1 indicando que não achou a chave}}
+    }*/
+
+public static int buscaMelhorada (int vet[ ], int procurado){
+    int i;
+    boolean achou = false;
+    for (i = 0; i < vet.length; i++)  {
+        if (vet[i] == procurado)  {
+            achou = true;
+            break;
+            }
+        else if (vet[i] > procurado) {
+            break;
+            }
+         }
+    if (achou == true) {
+        return i;     // retorna a posição onde encontrou a chave
+        } 
+        else {
+    return -1;  // retorna -1 indicando que não achou a chave}
+   }
     }
-    
 }
-    }
+    
